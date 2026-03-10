8cee7b871474af910f4a846d3bce74b427786344
mersimoihail <marsymoyhailu@gmail.com>
2026-03-10 03:18:30 -1200

    Time: 0 ms (100%), Space: 19.4 MB (23.54%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -6,14 +6,6 @@ class Solution:
             if x <= 0 or x % 4 != 0:
                 return False
             return power(x // 4)
-        def powerl(l):
-            if l == 1:
-                return True
-            if l >1 :
-                return False
-            else:
-                return power(l*4)
-        if n < 0:
-            return powerl(n)
-        else:
-            return(power(n))
+        
+        
+        return(power(n))
